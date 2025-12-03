3e72d68e6be772812ef262053dc392888507ea8a
op6160 <sagvd01@gmail.com>
2025-12-03 10:15:44 +0900

    feat: add a test_squash func

--- tests/get_contents_mhtml.py
+++ tests/get_contents_mhtml.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
-import email
 
+# ======================================================================================= #
+import email
 def parse_mhtml_to_soup(mhtml_content):
+    """tester code"""
     msg = email.message_from_string(mhtml_content)
     html_content = None
     # find text/html
@@ -23,17 +25,30 @@ def parse_mhtml_to_soup(mhtml_content):
 with open("tests/dev.test.mhtml", 'r', encoding='utf-8') as f:
     mhtml_text = f.read()
     soup = parse_mhtml_to_soup(mhtml_text)
+# ======================================================================================= #
+
+def text_squash(text):
+    """'Remove whitespace' and 'replace newlines to whitespace' from a string."""
+    text = text.replace(" ", "")
+    text = text.replace("\t", "")
+    text = text.replace("\n\n", " ")
+    text = text.replace("\n", " ")
+    return text
 
 rate_info = []
 for counting in range(20):
-    target = soup.find("div", id = f"unko_joho_shosai_{counting}_naiyo")
-    if target is None:
+    rate_info_div = soup.find("div", id = f"unko_joho_shosai_{counting}_naiyo")
+    if rate_info_div is None:
         break
-    target_soup = BeautifulSoup(target.prettify(), 'html.parser')
-    for tag in target_soup.find_all(["br"]):
+    rate_info_div_soup = BeautifulSoup(rate_info_div.prettify(), 'html.parser')
+    for tag in rate_info_div_soup.find_all(["br"]):
         tag.decompose()
-    rate_info.append(target_soup)
-    
-print(rate_info)
+    rate_info_div_soup_text = rate_info_div_soup.find("p").text
+    rate_info_div_soup_text = text_squash(rate_info_div_soup_text)
+    rate_info.append(rate_info_div_soup_text)
+
+# print(rate_info)
 print("*"*20)
-print(rate_info[0].find("p").text)
+print("rate_info: ")
+print(rate_info[0]) # .find("p").text
+print("*"*20)
