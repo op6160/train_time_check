6c6d1a3151bc70ddcc5478db1e7086b573c55fc5
op6160 <sagvd01@gmail.com>
2026-01-23 11:07:30 +0900

    refactor: import path change

--- src/libs.py
+++ src/libs.py
@@ -10,8 +10,12 @@ submodule_path = os.path.join(project_root, 'libs', 'utility_python')
 if submodule_path not in sys.path:
     sys.path.insert(0, submodule_path)
 
-import src.module.printer as printer
-from src.module.printer import log_msg as log
-from src.module.time_lib import detail
-from src.module.connection import headers, connection_test
-from src.module.browser import get_html_content
+from src.logger_lib import log_msg as log
+from src.time_lib import detail
+from src.browser_lib import headers, connection_test, get_html_content
+
+# import src.module.printer as printer
+# from src.module.printer import log_msg as log
+# from src.module.time_lib import detail
+# from src.module.connection import headers, connection_test
+# from src.module.browser import get_html_content
