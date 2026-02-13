6307668f03df20667b23b4c242d2dcfb28a3c1ee
op6160 <sagvd01@gmail.com>
2026-02-13 15:42:19 +0900

    chore: linux lf force apply

--- .ubuntu/schedule_morning.sh
+++ .ubuntu/schedule_morning.sh
@@ -1,7 +1,7 @@
 # crontab -e
 # chmod +x schedule_morning.sh
 # 30 7 * * 1-5 bash /home/ubuntu/schedule_morning.sh >> /home/ubuntu/cron.log 2>&1
-cd /home/ubuntu/services/train_time_check/.ubuntu
+cd /home/ubuntu/services/train_time_check/.ubuntu 
 ../venv/bin/python reusable_check.py \
  --target_station="金山" \
  --direction="down" \
