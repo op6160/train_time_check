dcbfe6b3043b076a188307b34786941e201085c3
op6160 <sagvd01@gmail.com>
2026-02-13 15:22:10 +0900

    fix: new line occured err /r

--- .ubuntu/schedule_morning.sh
+++ .ubuntu/schedule_morning.sh
@@ -2,7 +2,6 @@
 # chmod +x schedule_morning.sh
 # 30 7 * * 1-5 bash /home/ubuntu/schedule_morning.sh >> /home/ubuntu/cron.log 2>&1
 cd /home/ubuntu/services/train_time_check/.ubuntu
-
 ../venv/bin/python reusable_check.py \
  --target_station="金山" \
  --direction="down" \
