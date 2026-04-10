14752ea9dc36d994a98d7d4646fa52d14868be16
op6160 <sagvd01@gmail.com>
2026-04-10 17:52:35 +0900

    add: add new model package to define the logical data to replace dictionary type

---
+++ src/model/model.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+
+# @dataclass
+# class MessageDataModel:
+#     notice_message: str
+#     train_messages: list
+
+@dataclass
+class TrainDataModel:
+    id: str
+    train_level: int
+    direction: str
+    destination: str
+    before_passed_station: str
+    to_station: str
+    train_rate_time_str: str
+    arrived_station: str
+    before_station_id: str
+    next_stop: str
+    next_station_id: str
+
+@dataclass
+class NoticeDataModel:
+    go_down_title: str
+    go_down_info: list
+    go_up_title: str
+    go_up_info: list
+    _rate_info: list
+    state_title: str
+
+@dataclass
+class TrainDirectionMappingModel:
+    on_station: str
+    direction: str
+
+    def __post_init__(self):
+        self.on_station = "1" if self.on_station == "on" else "0"
+        self.direction = "r" if self.direction == "up" else "l"
+
+@dataclass
+class MessageDataModel:
+    is_train_state_normal: bool
+    notice_data: NoticeDataModel
+    train_data_list: list[TrainDataModel]
